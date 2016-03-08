0b14028741441e692fa3128b677e87cb6eadf0b0
Yeven <sunyawang1990@gmail.com>
2016-03-08 21:51:57 +0800

    添加监听
    添加监听

--- RunLoopPPTDemo/ViewController.m
+++ RunLoopPPTDemo/ViewController.m
@@ -20,8 +20,15 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    //监听RunLoop状态
+    CFRunLoopObserverContext context = {0, (__bridge void *)(self), NULL, NULL, NULL};
+    CFRunLoopObserverRef observer = CFRunLoopObserverCreate(kCFAllocatorDefault, kCFRunLoopAllActivities, YES, 0, &runLoopOberverCallBack, &context);
+    if (observer) {
+        CFRunLoopAddObserver(CFRunLoopGetCurrent(), observer, kCFRunLoopCommonModes);
+    }
 
-    _myTimer = [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(timerCallBack:) userInfo:nil repeats:YES];
+    _myTimer = [NSTimer scheduledTimerWithTimeInterval:2.0 target:self selector:@selector(timerCallBack:) userInfo:nil repeats:YES];
     
     //增加这行代码，则timer不受滑动tableview影响，没有这行代码则滑动tableview时定时器暂停
 //    CFRunLoopAddTimer(CFRunLoopGetCurrent(), (__bridge CFRunLoopTimerRef)_myTimer, kCFRunLoopCommonModes);
@@ -39,6 +46,19 @@
 - (void)timerCallBack:(NSTimer *)timer {
     static int flag = 0;
     _myTipLabel.text = @(flag++).stringValue;
+}
+
+void runLoopOberverCallBack(CFRunLoopObserverRef observer, CFRunLoopActivity activity, void *info) {
+    switch (activity) {
+        case kCFRunLoopEntry:
+            NSLog(@"进入RunLoop ，Mode:%@",(__bridge_transfer NSString*)CFRunLoopCopyCurrentMode(CFRunLoopGetCurrent()));
+            break;
+        case kCFRunLoopExit:
+            NSLog(@"退出RunLoop ，Mode:%@",(__bridge_transfer NSString*)CFRunLoopCopyCurrentMode(CFRunLoopGetCurrent()));
+            break;
+        default:  
+            break;  
+    }
 }
 
 
